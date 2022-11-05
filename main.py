06de91a8f969c4c6c507b10df4d3283f278e87de
GabrieLempert <81615014+GabrieLempert@users.noreply.github.com>
2022-11-05 16:45:48 +0100

    Changed to class structure code

--- main.py
+++ main.py
@@ -30,58 +30,73 @@ def computer_window(window):
     2 computers frame each will show there stats in the game
     """
 
-    # Computer 1
-    computer_1 = ComputerLayer(tk.Frame(window, width=200, borderwidth="2", relief=tk.RIDGE, height=200), 1)
-    computer_1.frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
-    computer_2 = ComputerLayer(tk.Frame(window, width=200, borderwidth="2", relief=tk.RIDGE, height=200), 2)
-    computer_2.frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 
-    computer_1.computer_header_layer(25)
-    computer_1.playing_grid()
-    # Computer 2
-    computer_2.computer_header_layer(25)
-    computer_2.playing_grid()
+class InfoLayer:
+    def __init__(self, frame, text):
+        self.frame = frame
+        self.text = text
 
+    def info_grid(self):
+        self.frame.columnconfigure([0, 1], weight=1, minsize=250)
+        self.frame.rowconfigure(0, weight=1, minsize=100)
+        label_computer = tk.Label(master=self.frame, text=self.text, width=40)
+        btn = tk.Button(master=self.frame, text="Start")
+        label_computer.grid(row=0, column=0)
+        btn.grid(row=0, column=1, sticky="nwse")
 
-def info_grid(frame, text):
-    frame.columnconfigure([0, 1], weight=1, minsize=250)
-    frame.rowconfigure(0, weight=1, minsize=100)
-    label_computer = tk.Label(master=frame, text=text, width=40)
-    btn = tk.Button(master=frame, text="Start")
-    label_computer.grid(row=0, column=0)
-    btn.grid(row=0, column=1, sticky="nwse")
+    def change_text(self, text):
+        self.text = text
 
 
-def main_window(window):
-    """
-    main window shows 2 computers and stats in bottom frame
-    """
-    top = tk.Frame(
-        master=window,
-        height=100,
-        border=1
-    )
-    top.pack(fill=tk.BOTH, expand=True)
-    computer_window(top)
+class MainWindow:
+    def __init__(self, top, bottom):
+        self.top = top
+        self.bottom = bottom
+        self.top.pack(fill=tk.BOTH, expand=True)
+        self.bottom.pack(fill=tk.BOTH)
 
-    bottom = tk.Frame(
-        master=window,
-        height=150,
-        relief=tk.RIDGE,
-        borderwidth=5
-    )
-    bottom.pack(fill=tk.BOTH)
-    who_won = ""
-    info_grid(bottom, f"Game Number: {1}\n" + who_won)
+    def top_window(self):
+        # Computer 1
+        computer_1 = ComputerLayer(tk.Frame(master=self.top, width=200, borderwidth="2", relief=tk.RIDGE, height=200),
+                                   1)
+        computer_1.frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+        computer_2 = ComputerLayer(tk.Frame(master=self.top, width=200, borderwidth="2", relief=tk.RIDGE, height=200),
+                                   2)
+        computer_2.frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+        computer_1.computer_header_layer(25)
+        computer_1.playing_grid()
+        # Computer 2
+        computer_2.computer_header_layer(25)
+        computer_2.playing_grid()
+
+    def bottom_window(self, text):
+        info = InfoLayer(frame=self.bottom, text=text)
+        info.info_grid()
 
 
 def main():
     """
     main function that runs the program
     """
+
     window = tk.Tk()
+    bottom = tk.Frame(
+        master=window,
+        height=150,
+        relief=tk.RIDGE,
+        borderwidth=2
+    )
+    top = tk.Frame(
+        master=window,
+        height=100,
+        border=1
+    )
+    number = 0
     window.geometry("800x800")
-    main_window(window)
+    main_window = MainWindow(top=top, bottom=bottom)
+    main_window.top_window()
+    main_window.bottom_window(text=f"Games Played {number}\nComputer Won {number}")
     window.mainloop()
 
 
